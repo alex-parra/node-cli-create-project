64d69a883451da9e89f8d53916c13445362eb10b
Alex Parra <a.parra@proprli.com>
2019-07-30 16:01:01 +0100

    Basic arguments

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,29 @@
+import arg from 'arg';
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install',
+    },
+    {
+      argv: rawArgs.slice(2), // skip first two rawArgs which are the bin paths of node and this command
+    }
+  );
+
+  return {
+    template: args._[0],
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    runInstall: args['--install'] || false,
+  };
+}
 
 export function cli(args) {
-
-  console.log(args);
-
+  let options = parseArgumentsIntoOptions(args);
+  console.log(options);
 }
