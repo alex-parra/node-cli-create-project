6c51d8e19b96717062f5bbda918cad84dedfb284
Alex Parra <a.parra@proprli.com>
2019-07-30 16:20:07 +0100

    Get missing arguments with 'inquirer'

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
 import arg from 'arg';
+import inquirer from 'inquirer';
+
+import TEMPLATES from './templates.js';
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
@@ -23,7 +26,32 @@ function parseArgumentsIntoOptions(rawArgs) {
   };
 }
 
-export function cli(args) {
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'JavaScript';
+
+  if (options.skipPrompts) {
+    return { ...options, template: options.template || defaultTemplate };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({ name: 'template', type: 'list', message: 'Select project template', choices: TEMPLATES, default: defaultTemplate });
+  }
+
+  if (!options.git) {
+    questions.push({ name: 'git', type: 'confirm', message: 'Initialize git repo?', default: false });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git,
+  };
+}
+
+export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
   console.log(options);
 }
---
+++ src/templates.js
@@ -0,0 +1,7 @@
+
+const TEMPLATES = [
+  'JavaScript',
+  'TypeScript'
+];
+
+export default TEMPLATES;
